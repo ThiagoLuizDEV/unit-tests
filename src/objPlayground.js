f0c90a12bfc331afbfbb1ecb8517af840af53570
Thiago <thiago-470@outlook.com>
2022-09-21 14:40:37 -0300

    requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,28 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const sum = number1 + number2;
+  const mult = number1 * number2;
+  const div = number1 / number2;
+  const sub = number1 - number2;
+     
+     const flutuar = {
+      sum: Math.floor(sum),
+      mult: Math.floor(mult),
+      div: Math.floor(div),
+      sub: Math.floor(sub) };
+      return flutuar;
+    };
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+}
+  if (type === 'values') {
+  return Object.values(object);
+}
+  return Object.entries(object);
+};
 
 module.exports = { calculator, arrayGenerator };
--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,10 +43,12 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined()
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toBe('object')
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
